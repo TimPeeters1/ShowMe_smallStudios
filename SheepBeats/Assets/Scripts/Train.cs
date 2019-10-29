32997d9c619936f2ca8f5c527aeb17da55da71f4
P4radigm <leon.vanoldenborgh@student.hku.nl>
2019-10-29 11:45:01 +0100

    Added train

---
+++ SheepBeats/Assets/Scripts/Train.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Train : MonoBehaviour
+{
+    public float Speed = 1f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<Sheep>())
+        {
+            StartCoroutine(GameManager.Instance.GameOver());
+        }
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, Speed);
+    }
+}
